aeae30b9302e15e93c71e301a6c99e92adfe908c
nerpt <99486064+nerpt@users.noreply.github.com>
2022-03-30 15:14:45 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="nerpt.net" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          _________________________
         </p>
       </main>
 
