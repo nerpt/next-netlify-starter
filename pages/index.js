252a3253d45f92f3d42d48bcad322da2effb8c64
nerpt <99486064+nerpt@users.noreply.github.com>
2022-03-30 19:08:25 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,6 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Nerptt</title>
-         <link rel="icon" type="image/x-icon" href="IMG_2627.jpg">
-      </Head>
-
-      <main>
-        <Header title="nerpt.nett" />
-        <p className="description">
-          ______________l___________
-        </p>
-      </main>
-
-      <Footer />
-    </div>
+    
   )
 }
